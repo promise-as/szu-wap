8c48ae30d34c85fe57fc9198af8ad4ba7ac85c8f
Hong Baojin <13809735212.163.com>
2020-07-30 18:29:48 +0800

    based finished

---
+++ js/index.js
@@ -0,0 +1,23 @@
+charset = 'UTF-8';
+$(function(){
+  // 五大优势
+  function strengthSwitch(head, cont, classname) {
+    $(head).each(function (i) {
+      var that = this;
+
+      $(that).click(function () {
+        $(that).addClass(classname).siblings().removeClass(classname);
+        $($(cont)[i]).addClass(classname).siblings().removeClass(classname);
+      })
+    });
+  }
+  strengthSwitch(".s-head .h-item", ".strength .c-item", "on");
+
+  // 轮播图
+  var mySwiper = new Swiper('.swiper-container', {
+    // autoplay: true,//可选选项，自动滑动
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+  
+  })
+});
